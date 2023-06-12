6fd5582d8ed6f192a193906e35f2a96f28517c06
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-06-12 23:49:57 +0200

    fat: routing for delete request & param middleware for id

--- server/modules/market/market.route.ts
+++ server/modules/market/market.route.ts
@@ -3,7 +3,9 @@ import { marketController } from "./market.controller";
 
 export const marketRouter: Router = express.Router();
 marketRouter.param("name", marketController.validateNameParam);
+marketRouter.param("id", marketController.validateIdParam);
 
 marketRouter.get('/all', marketController.getAllMarkets);
 marketRouter.get('/:name', marketController.getMarketByName);
 marketRouter.post('/', marketController.validateCreateMarketBody, marketController.createMarket);
+marketRouter.delete('/:id', marketController.deleteMarket)
