3d252fac8ca1924e1dec1c4818f54a680d21c766
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-06-12 21:28:31 +0200

    feat: request custom interface for market creation

--- server/interfaces/request.types.ts
+++ server/interfaces/request.types.ts
@@ -3,3 +3,18 @@ import { Request } from "express";
 export interface MarketNameReq extends Request {
     marketName ?: string;
 }
+
+export interface MarketCreateReq extends Request {
+
+    createInput ?: {
+        etablissement_type: string;
+        etablissement: string;
+        location: string;
+        address: string;
+        mail: string;
+    }
+}
+
+export interface MarketCreateBody<T> extends Request {
+    createInput: T;
+}
