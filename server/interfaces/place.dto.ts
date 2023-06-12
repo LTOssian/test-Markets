49aade7067414df0b55df3280993d108780a575c
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-06-12 23:19:45 +0200

    fix: type etbalissement_type with the enum

--- server/interfaces/place.dto.ts
+++ server/interfaces/place.dto.ts
@@ -1,6 +1,8 @@
+import {PlaceType} from "./place_type.enum";
+
 export interface PlaceDto {
     id: number,
-    etablissement_type: string,
+    etablissement_type: PlaceType,
     etablissement: string,
     location: string,
     address: string,
