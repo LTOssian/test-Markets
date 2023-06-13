e9c9594e0cf0248aa36aeda0bb2f3694d389664c
Louisan TCHITOULA <t.louisan@outlook.fr>
2023-06-13 15:00:54 +0200

    chore: remove unused imports

--- server/modules/field/field.service.ts
+++ server/modules/field/field.service.ts
@@ -1,6 +1,5 @@
 import {dataClient} from "../../utils/dbJSON";
 import {marketDto} from "../market/market.schema";
-import {PlaceType} from "../../interfaces/place_type.enum";
 
 export const fieldService = {
     getMarketByField: async(fieldParam: string): Promise<marketDto[]> => {
